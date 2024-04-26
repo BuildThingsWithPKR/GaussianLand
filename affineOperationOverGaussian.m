302e4e5e25c580e17c2751ae46ab27eab05c077f
BuildThingsWithPKR <157862225+BuildThingsWithPKR@users.noreply.github.com>
2024-04-26 18:30:14 +0530

    Create affineOperationOverGaussian.m

---
+++ affineOperationOverGaussian.m
@@ -0,0 +1,21 @@
+%% Signal Scaling and Offset
+L=5000; %number of samples assaigned
+mu1=2;
+sigma1 = 1; %standard deviation of smaples
+X1 = normrnd(mu1,sigma1,[1,L]);
+normalpd = fitdist(X1',"Normal")
+plot(normalpd)
+hold on
+
+X2 = 2*X1 + 3;
+normalpd = fitdist(X2',"Normal")
+plot(normalpd)
+
+
+legend({"X1: mu=2, sigma = 1  ","X1: mu=2, sigma = 1  ", ...
+    "X2 = 2*X1 + 3", "X2 = 2*X1 + 3"},...
+    'Location','northwest','NumColumns',2,FontSize=24)
+xlabel("Sample [N]", FontSize=24)
+ylabel("Value", FontSize=24)
+axis padded
+
